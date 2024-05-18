83cad8fb781f4e08b146bcab349e59c61b482cb4
DimaShcherbiuk <147422642+DimaShcherbiuk@users.noreply.github.com>
2024-05-18 10:10:25 +0300

    add componente

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from "react";
+
+// import css from "./App.module.css";
+import SearchBar from "./SearchBar/SearchBar";
+
+function App() {
+  return (
+    <>
+      <SearchBar />
+    </>
+  );
+}
+
+// 3tMI0COuLgOQinKGQmDcIpS3k-QYKUOKzBesCWiMRlg
+
+export default App;
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,11 @@
+import { Form, Formik } from "formik";
+
+function SearchBar() {
+  return (
+    <Formik>
+      <Form></Form>
+    </Formik>
+  );
+}
+
+export default SearchBar;
